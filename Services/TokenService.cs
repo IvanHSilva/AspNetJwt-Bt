06c292350a8dd5df24646e39092f585897e9833d
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-09-11 19:33:01 -0300

    Token assignment

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,6 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace JwtAspNet.Services; 
 
@@ -7,6 +9,12 @@ public class TokenService {
     public string CreateToken(){
         
         JwtSecurityTokenHandler handler = new();
+        
+        byte[] key = Encoding.UTF8.GetBytes(Configuration.PrivateKey);
+
+        new SigningCredentials(new SymmetricSecurityKey(key), 
+            SecurityAlgorithms.Sha256);
+
         JwtSecurityToken token = handler.CreateToken();
         return handler.WriteToken(token);
     }
