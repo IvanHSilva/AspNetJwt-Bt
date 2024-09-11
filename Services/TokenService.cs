c8e7ef2f332598051a9c047b251495e14ea61acf
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-09-11 19:27:29 -0300

    Token service

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,8 +1,13 @@
-﻿namespace JwtAspNet.Services; 
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace JwtAspNet.Services; 
 
 public class TokenService {
     
     public string CreateToken(){
-
+        
+        JwtSecurityTokenHandler handler = new();
+        JwtSecurityToken token = handler.CreateToken();
+        return handler.WriteToken(token);
     }
 }
