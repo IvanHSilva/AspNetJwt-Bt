3f0a516292f4a8d5cca147dbb755d8eb39ffc339
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-09-14 17:33:17 -0300

    Authentication configuration

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using JwtAspNet;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<TokenService>();
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(a => {
+    a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(a => {
+    a.TokenValidationParameters = new TokenValidationParameters {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.PrivateKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
