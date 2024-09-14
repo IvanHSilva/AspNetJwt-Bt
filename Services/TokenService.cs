d27bec2583d8e2e52612d27af9eceda58782d9b4
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-09-14 11:52:33 -0300

    ClaimsIdentity method

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,8 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Security.Claims;
+using JwtAspNet.Models;
 
 namespace JwtAspNet.Services; 
 
@@ -22,5 +24,20 @@ public class TokenService {
 
         SecurityToken token = handler.CreateToken(descriptor);
         return handler.WriteToken(token);
+    }
+
+    private ClaimsIdentity GenerateClaims(User user) {
+        
+        ClaimsIdentity claims = new();
+        claims.AddClaim(new Claim("Id", user.Id.ToString()));
+        claims.AddClaim(new Claim(ClaimTypes.Name, user.Email));
+        claims.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+        claims.AddClaim(new Claim(ClaimTypes.GivenName, user.Name));
+        claims.AddClaim(new Claim("Image", user.Image));
+
+        foreach (string role in user.Roles)
+            claims.AddClaim(new Claim(ClaimTypes.Role, role));
+
+        return claims;
     }
 }
